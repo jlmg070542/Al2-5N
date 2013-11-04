1910e318faa63d79341a2666a10e55ca3210f58d
JEFERSON LUIS MORAES GONÇALVES <jlmg070542@gmail.com>
2013-11-04 20:14:46 -0200

    Create AvaliadorRPN.java

---
+++ TrabalhoPilhas/AvaliadorRPN.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class AvaliadorRPN {
+	public static double avalia(String expressao) throws PilhaCheia,
+			PilhaVazia, InvalidOperator {
+		PilhaOperador pilha = new PilhaOperador(50);
+
+		Scanner sc = new Scanner(expressao);
+
+		while (sc.hasNext()) {
+			if (sc.hasNextInt()) {
+				pilha.push(sc.nextInt());
+			} else {
+				String op = sc.next();
+				double rhs = pilha.pop();
+				double lhs = pilha.pop();
+				pilha.push(executaOperador(op.charAt(0), lhs, rhs));
+			}
+		}
+
+		return pilha.pop();
+	}
+
+	private static double executaOperador(char op, double lhs, double rhs)
+			throws InvalidOperator {
+		switch (op) {
+		case '+':
+			return lhs + rhs;
+		case '-':
+			return lhs - rhs;
+		case '*':
+			return lhs * rhs;
+		case '/':
+			return lhs / rhs;
+		default:
+			throw new InvalidOperator(op);
+		}
+	}
+}
